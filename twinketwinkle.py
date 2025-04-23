3b2bec9daf1befd6810bf532b7071c0fad9ca2aa
Entropy-rgb <som3shkamad@gmail.com>
2025-04-23 13:56:33 +0000

    chapter 1

---
+++ twinketwinkle.py
@@ -0,0 +1,4 @@
+print('twinkle twinkle little star')
+print('how i wonder what you are')
+print('up above the world so high')
+print('like a diamond in the sky')
---
+++ using_os_module_for_the_first_time.py
@@ -0,0 +1,3 @@
+import os #import the os module
+cwd = os.listdir() #lists the files in this directory i.e. cwd
+print('The contents of this directory are :', cwd)  #finally prints its contents
